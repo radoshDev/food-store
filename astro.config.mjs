bdb316510be9b5945b444a69973b752eab59e17d
Oleksandr Radoshniuk <radosh.dev@gmail.com>
2023-06-12 18:29:30 +0300

    add header, footer and about

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import image from '@astrojs/image'
+import react from '@astrojs/react'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	experimental: {
+		assets: true,
+	},
+	integrations: [
+		react(),
+		image({
+			serviceEntryPoint: '@astrojs/image/sharp',
+		}),
+	],
+})
